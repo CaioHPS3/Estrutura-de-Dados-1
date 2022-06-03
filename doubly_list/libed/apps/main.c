e8380ca4362235e7e573b8c65410d9808e6720b3
CaioHPS3 <caiohenriqueps2030@gmail.com>
2022-06-03 17:23:15 -0300

    Atualização

--- doubly_list/libed/apps/main.c
+++ doubly_list/libed/apps/main.c
@@ -6,11 +6,9 @@ int main(){
   scanf("\n%4095s", buffer);
   TDList* num1 = TDList_generate(buffer);
   puts("");
-  TDList_print(num1);
   scanf("\n%4095s", buffer);
   TDList* num2 = TDList_generate(buffer);
   puts("");
-  TDList_print(num2);
   TDList* sum = TDList_sum(num1, num2);
   TDList_print(sum);
   TDList_destroy(&num1);
